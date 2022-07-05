af5d3ab5f0b52399137f07adcb19c8f941e7368f
Shaun Newman <shaun@newmaninbox.com>
2022-07-05 08:38:15 -0700

    nvim: moves packer_compiled to data dir so it's not version controlled

--- nvim/.config/nvim/lua/scnewma/plugins.lua
+++ nvim/.config/nvim/lua/scnewma/plugins.lua
@@ -1,25 +1,17 @@
--- recompile packer automatically on file change
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
 -- bootstrapping
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-return require('packer').startup(function (use)
+return require('packer').startup({ function (use)
     use 'wbthomason/packer.nvim'
     use 'sainnhe/gruvbox-material'
 
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use 'nvim-telescope/telescope-ui-select.nvim'
 
     use 'tpope/vim-commentary'
@@ -95,4 +87,5 @@ return require('packer').startup(function (use)
     if packer_bootstrap then
         require('packer').sync()
     end
-end)
+end, config = { compile_path = vim.fn.stdpath('data') .. '/plugin/packer_compiled.lua' }
+})
