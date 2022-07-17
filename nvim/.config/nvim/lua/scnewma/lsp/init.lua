af7d6ee10b6e1178a81d9bd956702d5509ee2807
Shaun Newman <newman350z@gmail.com>
2022-07-17 12:09:44 -0700

    improves rust lsp experience

--- nvim/.config/nvim/lua/scnewma/lsp/init.lua
+++ nvim/.config/nvim/lua/scnewma/lsp/init.lua
@@ -4,8 +4,6 @@ if not has_lsp then
 end
 
 local on_attach = function(_, bufnr)
-    local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
-
     local function map(tbl)
         if tbl[3] == nil then
             tbl[3] = {}
@@ -31,20 +29,20 @@ local on_attach = function(_, bufnr)
 
     map { '<Leader>cf', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>' }
     map { '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>' }
-
-    if vim.tbl_contains({"rust"}, filetype) then
-        vim.cmd [[autocmd BufWritePre <buffer> :lua vim.lsp.buf.formatting_sync(nil, 1000)]]
-    end
 end
 
--- config that activates keymaps and enabled snippet support
-local function make_config()
+local function make_capabilities()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.snippetSupport = true
     capabilities.textDocument.codeLens = { dynamicRegistration = false }
     capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+    return capabilities
+end
+
+-- config that activates keymaps and enabled snippet support
+local function make_config()
     return {
-        capabilities = capabilities,
+        capabilities = make_capabilities(),
         on_attach = on_attach,
     }
 end
@@ -56,7 +54,6 @@ lspconfig.gopls.setup(make_config())
 lspconfig.jsonls.setup(make_config())
 lspconfig.pyright.setup(make_config())
 lspconfig.rnix.setup(make_config())
-lspconfig.rust_analyzer.setup(make_config())
 lspconfig.vimls.setup(make_config())
 lspconfig.yamlls.setup(make_config())
 
@@ -77,6 +74,44 @@ local lua_config = vim.tbl_deep_extend("force", make_config(), {
   },
 })
 lspconfig.sumneko_lua.setup(lua_config)
+
+require('rust-tools').setup({
+    tools = {
+        autoSetHints = false,
+    },
+
+    server = {
+        on_attach = function(client, bufnr)
+            -- call default on_attach
+            on_attach(client, bufnr)
+
+            local function map(tbl)
+                if tbl[3] == nil then
+                    tbl[3] = {}
+                end
+                tbl[3].silent = true
+                tbl[3].noremap = true
+                tbl[3].buffer = bufnr
+                vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
+            end
+
+            -- overrside 'K' to a rust-specific version
+            map { 'K', require('rust-tools.hover_actions').hover_actions }
+
+            -- open "module" description (i.e. Cargo.toml)
+            map { '<leader>cm', require('rust-tools.open_cargo_toml').open_cargo_toml }
+
+            -- rust-aware join lines
+            map { 'J', require('rust-tools.join_lines').join_lines }
+
+            -- toggle inlay hints as they are disabled by default
+            map { '<leader>th', require('rust-tools.inlay_hints').toggle_inlay_hints }
+
+            vim.cmd [[autocmd BufWritePre <buffer> :lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+        end,
+        capabilities = make_capabilities(),
+    }
+})
 
 require("fidget").setup({})
 
