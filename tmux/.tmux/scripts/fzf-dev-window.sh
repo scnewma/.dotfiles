5f49d57102165918a70e67fe2452a3938a130da0
Shaun Newman <newman350z@gmail.com>
2023-07-25 21:41:30 -0700

    adds cmd-p to alacritty + tmux; fix exit on ctrl-c
    fixes a long-standing bug where exiting the popup window would take you
    to an intermediate pane with an exit code.

--- tmux/.tmux/scripts/fzf-dev-window.sh
+++ tmux/.tmux/scripts/fzf-dev-window.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-set -euo pipefail
-
 # select any project from ~/dev and adds the ~/.dotfiles directory
 dir="$(
     (fd . -d1 -td "${HOME}/dev"; echo "${HOME}/.dotfiles") \
         | fzf-tmux -p --header='Select project.')"
 
-[ -z "${dir}" ] && return 0
+[ -z "${dir}" ] && exit 0
 
 window_name="$(basename "${dir}" | sed 's/\./_/g')"
 tmux new-window -c "${dir}" -n "${window_name}"
