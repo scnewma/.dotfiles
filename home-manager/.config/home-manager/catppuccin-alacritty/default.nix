4a0bbc061cc499d65ec27a8ca31d175524ac3f2b
Shaun Newman <newman350z@gmail.com>
2024-02-01 13:57:54 -0700

    alacritty: migrate to toml configuration
    necessary because of the new alacritty version

--- home-manager/.config/home-manager/catppuccin-alacritty/default.nix
+++ home-manager/.config/home-manager/catppuccin-alacritty/default.nix
@@ -6,13 +6,13 @@ stdenv.mkDerivation rec {
   src = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "alacritty";
-    rev = "3c808cbb4f9c87be43ba5241bc57373c793d2f17";
-    sha256 = "w9XVtEe7TqzxxGUCDUR9BFkzLZjG8XrplXJ3lX6f+x0=";
+    rev = "f2da554ee63690712274971dd9ce0217895f5ee0";
+    sha256 = "sha256-ypYaxlsDjI++6YNcE+TxBSnlUXKKuAMmLQ4H74T/eLw=";
   };
 
   installPhase = ''
     dir=$out/share/catppuccin-alacritty
     mkdir -p $dir
-    cp *.yml $dir/
+    cp *.toml $dir/
   '';
 }
