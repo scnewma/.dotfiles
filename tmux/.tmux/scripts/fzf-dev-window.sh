2b02f8ee607effbecf2e8c6fd9194bd202ef67e4
Shaun Newman <shaun.newman@hashicorp.com>
2025-02-10 11:48:20 -0700

    tmux: increase project selection popup width
    When using small window sizes, the popup is unusable otherwise.

--- tmux/.tmux/scripts/fzf-dev-window.sh
+++ tmux/.tmux/scripts/fzf-dev-window.sh
@@ -3,7 +3,7 @@
 # select any project from ~/dev and adds the ~/.dotfiles directory
 dir="$(
     (fd . -d1 -td "${HOME}/dev"; echo "${HOME}/.dotfiles") \
-        | fzf-tmux -p --header='Select project.')"
+        | fzf-tmux -p '90%' --header='Select project.')"
 
 [ -z "${dir}" ] && exit 0
 
