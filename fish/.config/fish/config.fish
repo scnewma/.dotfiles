41bf4bc4f297adb223d3cb6da2ee6e5d69399c5e
Shaun Newman <newman350z@gmail.com>
2025-03-08 18:50:18 -0700

    fish: add more jj abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -144,7 +144,21 @@ if status --is-interactive
 
     abbr -ag jl jj git fetch
     abbr -ag jp jj git push
+    abbr -ag jp@ jj git push -c @
+    abbr -ag jc jj commit
+    abbr -ag jf jj diff
+    abbr -ag jd jj describe
+    abbr -ag jdm jj describe -m
+    abbr -ag je jj edit
+    abbr -ag jlg jj log
+    abbr -ag jrs jj restore
+    abbr -ag jsh jj show
+    abbr -ag jsp jj split
+    abbr -ag jsq jj squash
+    abbr -ag jst jj status
+    abbr -ag jb jj bookmark
     abbr -ag jbm "jj bookmark set (jj-main-bookmark)"
+    abbr -ag jbmp "jj bookmark set (jj-main-bookmark); and jj git push -b (jj-main-bookmark)"
 end
 
 # Environment variables from zsh
