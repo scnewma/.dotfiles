c955a304c0d5d89d68a75c9a9e1b1a2a21b9b1e5
Shaun Newman <shaun.newman@hashicorp.com>
2025-03-08 18:11:10 -0700

    nvim: switch to fzf-lua

--- nvim/.config/nvim/lua/scnewma/plugins/init.lua
+++ nvim/.config/nvim/lua/scnewma/plugins/init.lua
@@ -20,31 +20,54 @@ return {
 
     'nvim-lua/popup.nvim',
     'nvim-lua/plenary.nvim',
-    'junegunn/fzf',
     {
-        'junegunn/fzf.vim',
-        config = function ()
-            local function set_quickfix(files)
-                local qf = {}
-                for _, filename in pairs(files) do
-                    table.insert(qf, { filename = filename })
-                end
-                vim.fn.setqflist(qf)
-                vim.cmd [[ copen ]]
-            end
-
-            vim.env.FZF_DEFAULT_OPTS = vim.env.FZF_DEFAULT_OPTS .. " --bind ctrl-a:select-all --bind ctrl-d:deselect-all"
-            vim.g.fzf_action = {
-                -- defaults
-                ['ctrl-t'] = 'tab split',
-                ['ctrl-x'] = 'split',
-                ['ctrl-v'] = 'vsplit',
-
-                ['alt-q'] = set_quickfix,
-            }
+        "ibhagwan/fzf-lua",
+        cmd = "FzfLua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            defaults = {
+                file_icons = false,
+                color_icons = false,
+            },
+        },
+        keys = {
+            { '<leader>.', '<cmd>:FzfLua resume<CR>' },
+            { '<leader>:', '<cmd>:FzfLua command_history<CR>' },
+            { '<leader>/', '<cmd>:FzfLua live_grep<CR>' },
+            { '<leader>*', '<cmd>:FzfLua grep_cword<CR>' },
+            { '<leader><space>', '<cmd>:FzfLua files<CR>' },
+            { '<leader>fg', '<cmd>:FzfLua git_files<CR>' },
+            { '<leader>fe', '<cmd>:FzfLua files cwd=~/.dotfiles<CR>' },
+            { '<leader>bb', '<cmd>:FzfLua buffers<CR>' },
+            { '<leader>ss', '<cmd>:FzfLua blines<CR>' },
+            { 'gd', '<cmd>:FzfLua lsp_definitions<CR>' },
+            { 'gD', '<cmd>:FzfLua lsp_declarations<CR>' },
+            { 'gA', '<cmd>:FzfLua lsp_references<CR>' },
+            { 'gy', '<cmd>:FzfLua lsp_typedefs<CR>' },
+            { 'gI', '<cmd>:FzfLua lsp_implementations<CR>' },
+            { 'gs', '<cmd>:FzfLua lsp_document_symbols<CR>' },
+            { 'gS', '<cmd>:FzfLua lsp_live_workspace_symbols<CR>' },
+            { 'g.', '<cmd>:FzfLua lsp_code_actions<CR>' },
+            -- search
+            { '<leader>s"', "<cmd>FzfLua registers<cr>", desc = "Registers" },
+            { "<leader>sa", "<cmd>FzfLua autocmds<cr>", desc = "Auto Commands" },
+            { "<leader>sb", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
+            { "<leader>sC", "<cmd>FzfLua commands<cr>", desc = "Commands" },
+            { "<leader>sd", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document Diagnostics" },
+            { "<leader>sD", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
+            { "<leader>sh", "<cmd>FzfLua help_tags<cr>", desc = "Help Pages" },
+            { "<leader>sH", "<cmd>FzfLua highlights<cr>", desc = "Search Highlight Groups" },
+            { "<leader>sj", "<cmd>FzfLua jumps<cr>", desc = "Jumplist" },
+            { "<leader>sk", "<cmd>FzfLua keymaps<cr>", desc = "Key Maps" },
+            { "<leader>sl", "<cmd>FzfLua loclist<cr>", desc = "Location List" },
+            { "<leader>sM", "<cmd>FzfLua man_pages<cr>", desc = "Man Pages" },
+            { "<leader>sm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" },
+            { "<leader>sq", "<cmd>FzfLua quickfix<cr>", desc = "Quickfix List" },
+        },
+        init = function()
+            require("fzf-lua").register_ui_select()
         end,
     },
-    'gfanto/fzf-lsp.nvim',
 
     'tpope/vim-commentary',
     'tpope/vim-repeat',
