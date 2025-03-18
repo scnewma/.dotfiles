c568aa10699813a38c7be91eafbca8d3b02b89b7
Shaun Newman <newman350z@gmail.com>
2025-03-17 22:07:39 -0700

    try aerospace instead of paperwm.spoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,7 +1,7 @@
 package.path = package.path .. ";" ..  os.getenv("HOME") .. "/.nix-profile/share/Spoons/?.spoon/init.lua"
 
-hs.window.animationDuration = 0
-PaperWM = hs.loadSpoon("PaperWM")
-PaperWM.window_gap = 2
-PaperWM:bindHotkeys(PaperWM.default_hotkeys)
-PaperWM:start()
+-- hs.window.animationDuration = 0
+-- PaperWM = hs.loadSpoon("PaperWM")
+-- PaperWM.window_gap = 2
+-- PaperWM:bindHotkeys(PaperWM.default_hotkeys)
+-- PaperWM:start()
