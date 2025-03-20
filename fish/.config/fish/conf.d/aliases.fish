6a5e632a359adfe6be355e63525e0c444107b1a6
Shaun Newman <shaun.newman@hashicorp.com>
2025-03-20 09:20:39 -0700

    move jj aliases into jj.fish

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -108,23 +108,3 @@ abbr -ag dcomd docker-compose down
 abbr -ag dcomlo docker-compose logs -f
 abbr -ag dcomlg docker-compose logs
 abbr -ag dcomlgf docker-compose logs -f
-
-abbr -ag jl jj git fetch
-abbr -ag jp jj git push
-abbr -ag jp@ jj git push -c @
-abbr -ag jc jj commit
-abbr -ag jf jj diff
-abbr -ag jd jj describe
-abbr -ag jdm jj describe -m
-abbr -ag je jj edit
-abbr -ag jlg jj log
-abbr -ag jrs jj restore
-abbr -ag jsh jj show
-abbr -ag jsp jj split
-abbr -ag jsq jj squash
-abbr -ag jst jj status
-abbr -ag jb jj bookmark
-abbr -ag jbm "jj bookmark set (jj-main-bookmark)"
-abbr -ag jbmp "jj bookmark set (jj-main-bookmark); and jj git push -b (jj-main-bookmark)"
-abbr -ag jn jj new
-abbr -ag jnm "jj new (jj-main-bookmark)"
--- fish/.config/fish/conf.d/jj.fish
+++ fish/.config/fish/conf.d/jj.fish
@@ -1,2 +1,23 @@
 set -x JJ_CONFIG "$HOME/.dotfiles/jj/config"
 COMPLETE=fish jj | source
+
+abbr -ag jl jj git fetch
+abbr -ag jp jj git push
+abbr -ag jp@ jj git push -c @
+abbr -ag jc jj commit
+abbr -ag jf jj diff
+abbr -ag jd jj describe
+abbr -ag jdm jj describe -m
+abbr -ag je jj edit
+abbr -ag jlg jj log
+abbr -ag jrs jj restore
+abbr -ag jsh jj show
+abbr -ag jsp jj split
+abbr -ag jsq jj squash
+abbr -ag jst jj status
+abbr -ag jb jj bookmark
+abbr -ag jbs jj bookmark set
+abbr -ag jbm "jj bookmark set (jj-main-bookmark)"
+abbr -ag jbmp "jj bookmark set (jj-main-bookmark); and jj git push -b (jj-main-bookmark)"
+abbr -ag jn jj new
+abbr -ag jnm "jj new (jj-main-bookmark)"
