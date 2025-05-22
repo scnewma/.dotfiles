9e81fa0510dc5b87cddbaa4d19faa1182c00b1ce
Shaun Newman <shaun.newman@hashicorp.com>
2025-05-22 08:55:59 -0700

    fish: adds abbr for terraform apply --auto-approve

--- fish/.config/fish/conf.d/terraform.fish
+++ fish/.config/fish/conf.d/terraform.fish
@@ -3,4 +3,5 @@ type -q terraform || return
 abbr -ag tf terraform
 abbr -ag tfi terraform init
 abbr -ag tfa terraform apply
+abbr -ag tfaa terraform apply --auto-approve
 abbr -ag tfp terraform plan
