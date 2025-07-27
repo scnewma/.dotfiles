e5e3f97243f9810855c8ca7612f82b2e8a55c042
Shaun Newman <newman350z@gmail.com>
2025-07-27 13:09:02 -0700

    nvim: switch to vim.lsp.config/enable
    Switching lspconfig.setup() calls to vim.lsp.enable and vim.lsp.config
    which became available in nvim 0.11.x.
    
    There may still be some issues that I haven't discovered yet!
    
    Removed rust-tools for now since it was archived. They recommend
    switching to https://github.com/mrcjkb/rustaceanvim, but I didn't have
    time to look into that yet.
    
    Als, adds `ty` as an lsp server.

--- nvim/.config/nvim/lua/scnewma/plugins/lsp.lua
+++ nvim/.config/nvim/lua/scnewma/plugins/lsp.lua
@@ -1,51 +1,3 @@
-local on_attach = function(_, bufnr)
-    local function map(tbl)
-        if tbl[3] == nil then
-            tbl[3] = {}
-        end
-        tbl[3].silent = true
-        tbl[3].noremap = true
-        tbl[3].buffer = bufnr
-        vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
-    end
-
-    -- Mappings
-    map { 'gD', vim.lsp.buf.declaration }
-    map { 'K', vim.lsp.buf.hover }
-    vim.keymap.set('i', '<C-k>',
-        vim.lsp.buf.signature_help,
-        { silent = true, noremap = true, buffer = bufnr })
-
-    -- code-rename
-    map { '<Leader>cr', vim.lsp.buf.rename }
-
-    --  diagnostics
-    map { '<Leader>cl', vim.diagnostic.setloclist }
-    map { '[d', vim.diagnostic.goto_prev }
-    map { ']d', vim.diagnostic.goto_next }
-
-    map { '<Leader>cf', function() vim.lsp.buf.format({ async = false }) end }
-end
-
-local function make_capabilities()
-    local capabilities = require('blink.cmp').get_lsp_capabilities({
-        textDocument = {
-            codeLens = {
-                dynamicRegistration = false,
-            },
-        },
-    })
-    return capabilities
-end
-
--- config that activates keymaps and enabled snippet support
-local function make_config()
-    return {
-        capabilities = make_capabilities(),
-        on_attach = on_attach,
-    }
-end
-
 return {
     {
         'neovim/nvim-lspconfig',
@@ -74,6 +26,7 @@ return {
                     }
                 },
                 ruff = {},
+                ty = {},
                 vimls = {},
                 yamlls = {},
                 denols = {},
@@ -98,142 +51,66 @@ return {
                 gopls = {},
                 gleam = {},
             },
-            setup = {
-                denols = function(opts)
-                    opts = vim.tbl_deep_extend('force', opts, {
-                        root_dir = require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc'),
-                    })
-                    require('lspconfig').denols.setup(opts)
-                end,
-                ts_ls = function(opts)
-                    opts = vim.tbl_deep_extend('force', opts, {
-                        root_dir = require('lspconfig').util.root_pattern('package.json'),
-                        single_file_support = false,
-                    })
-                    require('lspconfig').ts_ls.setup(opts)
-                end,
-                gopls = function(opts)
-                    opts = vim.tbl_deep_extend('force', opts, {
-                        on_attach = function(client, bufnr)
-                            -- call default on_attach
-                            on_attach(client, bufnr)
-
-                            local function map(tbl)
-                                if tbl[3] == nil then
-                                    tbl[3] = {}
-                                end
-
-                                if tbl[3]['silent'] == nil then
-                                    tbl[3].silent = true
-                                end
-
-                                tbl[3].noremap = true
-                                tbl[3].buffer = bufnr
-                                vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
-                            end
-
-                            map { '<leader>mf', require("go.reftool").fillstruct }
-                            map { '<leader>mi', ':GoImpl' }
-                            -- TODO: I wish this didn't include the type on end of comment, but
-                            -- that's hardcoded in the plugin...
-                            map { '<leader>mc', require('go.comment').gen }
-                            map { '<leader>mta', ':GoAddTag json', { silent = false } }
-                            map { '<leader>mtr', ':GoRmTag json', { silent = false } }
-                            map { '<leader>mtc', ':GoClearTag', { silent = false } }
-                        end,
-
-                        ['local'] = require('scnewma.lsp.go').go_module_name(),
-                        analyses = {
-                            nilness = true,
-                            unusedparams = true,
-                            unusedwrite = true,
-                        },
-                    })
-
-                    require('lspconfig').gopls.setup(opts)
-                    require('go').setup({
-                        comment_placeholder = '',
-                        build_tags = "integration",
-                    })
-                end,
-                ruff = function(opts)
-                    opts = vim.tbl_deep_extend('force', opts, {
-                        on_attach = function(client, bufnr)
-                            -- call default on_attach
-                            on_attach(client, bufnr)
-
-                            if client.name == 'ruff' then
-                                -- disable hover in favor of pyright
-                                client.server_capabilities.hoverProvider = false
-                            end
-                        end,
-                    })
-                    require('lspconfig').ruff.setup(opts)
-                end,
-            },
         },
         config = function(_, opts)
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not client then
+                        return
+                    end
+
+                    local kmopts = {
+                        silent = true,
+                        noremap = true,
+                        buffer = args.buf,
+                    }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, kmopts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, kmopts)
+                    -- code-rename
+                    vim.keymap.set('n', '<Leader>cr', vim.lsp.buf.rename, kmopts)
+                    --  diagnostics
+                    vim.keymap.set('n', '<Leader>cl', vim.diagnostic.setloclist, kmopts)
+                    vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count=-1, float=true}) end, kmopts)
+                    vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count=1, float=true}) end, kmopts)
+
+                    vim.keymap.set('n', '<Leader>cf', function() vim.lsp.buf.format({ async = false }) end, kmopts)
+
+                    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, kmopts)
+
+                    if client.name == 'gopls' then
+                        vim.keymap.set('n', '<leader>mf', require("go.reftool").fillstruct, kmopts)
+                        vim.keymap.set('n', '<leader>mi', ':GoImpl', kmopts)
+                        -- TODO: I wish this didn't include the type on end of comment, but
+                        -- that's hardcoded in the plugin...
+                        vim.keymap.set('n', '<leader>mc', require('go.comment').gen, kmopts)
+                        vim.keymap.set('n', '<leader>mta', ':GoAddTag json', { silent = false, noremap = true, buffer = args.buf })
+                        vim.keymap.set('n', '<leader>mtr', ':GoRmTag json', { silent = false, noremap = true, buffer = args.buf })
+                        vim.keymap.set('n', '<leader>mtc', ':GoClearTag', { silent = false, noremap = true, buffer = args.buf })
+                    elseif client.name == "ruff" then
+                        -- Disable hover in favor of Pyright
+                        client.server_capabilities.hoverProvider = false
+                    end
+                end,
+            })
+
             local servers = opts.servers
             for server, server_opts in pairs(servers) do
                 server_opts = server_opts == true and {} or server_opts
-                server_opts = vim.tbl_deep_extend('force', make_config(), server_opts)
-                if opts.setup[server] then
-                    opts.setup[server](server_opts)
-                else
-                    require('lspconfig')[server].setup(server_opts)
-                end
+                vim.lsp.config[server] = server_opts
+                vim.lsp.enable(server)
             end
         end,
     },
 
     {
-        'simrat39/rust-tools.nvim',
+        "ray-x/go.nvim",
         dependencies = {
-            'j-hui/fidget.nvim',
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
         },
-        ft = 'rust',
-        opts = {
-            tools = {
-                autoSetHints = false,
-            },
-
-            server = {
-                on_attach = function(client, bufnr)
-                    -- call default on_attach
-                    on_attach(client, bufnr)
-
-                    local function map(tbl)
-                        if tbl[2] == nil then
-                            return
-                        end
-
-                        if tbl[3] == nil then
-                            tbl[3] = {}
-                        end
-                        tbl[3].silent = true
-                        tbl[3].noremap = true
-                        tbl[3].buffer = bufnr
-                        vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
-                    end
-
-                    -- overrside 'K' to a rust-specific version
-                    map { 'K', require('rust-tools.hover_actions').hover_actions }
-
-                    -- open "module" description (i.e. Cargo.toml)
-                    map { '<leader>cm', require('rust-tools.open_cargo_toml').open_cargo_toml }
-
-                    -- rust-aware join lines
-                    map { 'J', require('rust-tools.join_lines').join_lines }
-
-                    -- inlay hint management
-                    map { '<leader>mhe', require('rust-tools.inlay_hints').set }
-                    map { '<leader>mhd', require('rust-tools.inlay_hints').unset }
-
-                    vim.cmd [[autocmd BufWritePre <buffer> :lua vim.lsp.buf.format({ timeout_ms = 1000 })]]
-                end,
-                capabilities = make_capabilities(),
-                cmd = { "rustup", "run", "stable", "rust-analyzer" },
-            }
-        }
-    }
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()' -- install/update all binaries
+    },
 }
