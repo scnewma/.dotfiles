f82f73991da68b4eb400c915b6fc7f5bfaf9532f
Shaun Newman <shaun.newman@hashicorp.com>
2025-08-05 15:42:07 -0700

    nvim: move automatic formatting to lsp setup

--- nvim/.config/nvim/after/ftplugin/go.vim
+++ nvim/.config/nvim/after/ftplugin/go.vim
@@ -1,11 +1,3 @@
-" command! -nargs=* -range GoAddTags lua require('go-tools').add_tags({<line1>, <line2>, <f-args>})
-" command! -nargs=* -range GoRemoveTags lua require('go-tools').remove_tags({<line1>, <line2>, <f-args>})
-
-augroup go-format-on-save
-    autocmd!
-    autocmd BufWritePre *.go :silent! lua require("go.format").goimports()
-augroup END
-
 nmap <Leader>faa :GoAlt<CR>
 nmap <Leader>fas :GoAltS<CR>
 nmap <Leader>fav :GoAltV<CR>
--- nvim/.config/nvim/lua/scnewma/plugins/lsp.lua
+++ nvim/.config/nvim/lua/scnewma/plugins/lsp.lua
@@ -115,6 +115,17 @@ return {
             "neovim/nvim-lspconfig",
             "nvim-treesitter/nvim-treesitter",
         },
+        config = function(lp, opts)
+            require("go").setup(opts)
+            local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.go",
+                callback = function()
+                    require('go.format').goimports()
+                end,
+                group = format_sync_grp,
+            })
+        end,
         event = {"CmdlineEnter"},
         ft = {"go", 'gomod'},
         build = ':lua require("go.install").update_all_sync()' -- install/update all binaries
