e6cd318ed8cfd0ee120b4ed1a6ad981a95d79179
Shaun Newman <shaun.newman@hashicorp.com>
2026-04-16 11:05:59 -0500

    nvim: migrate treesitter to 0.12 main-branch setup
    Switch nvim-treesitter and nvim-treesitter-textobjects to their matching main branches and replace the old configs.setup-based config with the new 0.12 setup. This keeps markdown highlighting working on Neovim 0.12, re-establishes textobject mappings with the rewritten textobjects API, and records that Homebrew now requires tree-sitter-cli in addition to the tree-sitter library formula for :TSUpdate to build parsers.

--- nvim/.config/nvim/lua/scnewma/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/scnewma/plugins/treesitter.lua
@@ -1,117 +1,155 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        branch = 'main',
+        lazy = false,
         build = ':TSUpdate',
-        event = { 'BufReadPost', 'BufNewFile' },
         dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
+            {
+                'nvim-treesitter/nvim-treesitter-textobjects',
+                branch = 'main',
+                config = function()
+                    require('nvim-treesitter-textobjects').setup {
+                        select = {
+                            lookahead = true,
+                        },
+                        move = {
+                            set_jumps = true,
+                        },
+                    }
+
+                    local select = require('nvim-treesitter-textobjects.select')
+                    local move = require('nvim-treesitter-textobjects.move')
+                    local swap = require('nvim-treesitter-textobjects.swap')
+
+                    local select_keymaps = {
+                        af = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ac = '@conditional.outer',
+                        ic = '@conditional.inner',
+                        al = '@loop.outer',
+                        il = '@loop.inner',
+                        ['as'] = '@statement.outer',
+                        ['is'] = '@statement.inner',
+                        am = '@call.outer',
+                        im = '@call.inner',
+                        ap = '@parameter.outer',
+                        ip = '@parameter.inner',
+                    }
+
+                    for lhs, query in pairs(select_keymaps) do
+                        vim.keymap.set({ 'x', 'o' }, lhs, function()
+                            select.select_textobject(query, 'textobjects')
+                        end)
+                    end
+
+                    vim.keymap.set('n', '<leader>csa', function()
+                        swap.swap_next('@parameter.inner', 'textobjects')
+                    end)
+                    vim.keymap.set('n', '<leader>csA', function()
+                        swap.swap_previous('@parameter.inner', 'textobjects')
+                    end)
+
+                    vim.keymap.set({ 'n', 'x', 'o' }, ']m', function()
+                        move.goto_next_start('@function.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, ']]', function()
+                        move.goto_next_start('@class.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, ']M', function()
+                        move.goto_next_end('@function.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, '][', function()
+                        move.goto_next_end('@class.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, '[m', function()
+                        move.goto_previous_start('@function.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, '[[', function()
+                        move.goto_previous_start('@class.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, '[M', function()
+                        move.goto_previous_end('@function.outer', 'textobjects')
+                    end)
+                    vim.keymap.set({ 'n', 'x', 'o' }, '[]', function()
+                        move.goto_previous_end('@class.outer', 'textobjects')
+                    end)
+                end,
+            },
         },
         config = function()
-          vim.filetype.add({
-            extension = {
-              gotmpl = 'gotmpl',
-            },
-            pattern = {
-              [".*/templates/.*%.tpl"] = "helm",
-              [".*/templates/.*%.ya?ml"] = "helm",
-              ["helmfile.*%.ya?ml"] = "helm",
-            },
-          })
+            vim.filetype.add({
+                extension = {
+                    gotmpl = 'gotmpl',
+                },
+                pattern = {
+                    [".*/templates/.*%.tpl"] = 'helm',
+                    [".*/templates/.*%.ya?ml"] = 'helm',
+                    ["helmfile.*%.ya?ml"] = 'helm',
+                },
+            })
 
-          require("nvim-treesitter.configs").setup {
-            auto_install = true,
-            ensure_installed = {
-                'c',
+            local languages = {
                 'bash',
+                'c',
+                'dockerfile',
                 'go',
                 'gomod',
+                'gotmpl',
+                'hcl',
+                'helm',
+                'java',
                 'json',
                 'lua',
-                'python',
-                'dockerfile',
-                'hcl',
-                'java',
-                'ruby',
-                'rust',
-                'yaml',
-                'vim',
-                'vimdoc',
-                'zig',
-                'query',
                 'markdown',
                 'markdown_inline',
-            },
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-
-            indent = {
-                enable = true,
-                disable = { 'go' }
-            },
-
-            textobjects = {
-                select = {
-                    enable = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@conditional.outer",
-                        ["ic"] = "@conditional.inner",
-                        ["al"] = "@loop.outer",
-                        ["il"] = "@loop.inner",
-                        ["as"] = "@statement.outer",
-                        ["is"] = "@statement.inner",
-                        ["am"] = "@call.outer",
-                        ["im"] = "@call.inner",
-                        ["ap"] = "@parameter.outer",
-                        ["ip"] = "@parameter.inner",
-                    }
-                },
-
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>csa"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>csA"] = "@parameter.inner",
-                    }
-                },
-
-                move = {
-                    enable = true,
-                    goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
-                    },
-                    goto_next_end = {
-                        ["]M"] = "@function.outer",
-                        ["]["] = "@class.outer",
-                    },
-                    goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
-                    },
-                    goto_previous_end = {
-                        ["[M"] = "@function.outer",
-                        ["[]"] = "@class.outer",
-                    }
-                },
-
-                lsp_interop = {
-                    enable = true,
-                    peek_definition_code = {
-                        -- code-show-function
-                        ["<leader>csf"] = "@function.outer",
-                        -- code-show-class
-                        ["<leader>csc"] = "@class.outer",
-                    },
-                },
+                'python',
+                'query',
+                'ruby',
+                'rust',
+                'vim',
+                'vimdoc',
+                'yaml',
+                'zig',
             }
-        }
+
+            require('nvim-treesitter').install(languages)
+
+            local group = vim.api.nvim_create_augroup('ScnewmaTreesitter', { clear = true })
+            vim.api.nvim_create_autocmd('FileType', {
+                group = group,
+                pattern = {
+                    'bash',
+                    'c',
+                    'dockerfile',
+                    'go',
+                    'gomod',
+                    'gotmpl',
+                    'hcl',
+                    'helm',
+                    'java',
+                    'json',
+                    'lua',
+                    'markdown',
+                    'python',
+                    'query',
+                    'ruby',
+                    'rust',
+                    'vim',
+                    'vimdoc',
+                    'yaml',
+                    'zig',
+                },
+                callback = function(args)
+                    if not pcall(vim.treesitter.start, args.buf) then
+                        return
+                    end
+
+                    if vim.bo[args.buf].filetype ~= 'go' then
+                        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
+            })
         end,
     },
 }
